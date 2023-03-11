3f9046c6ecba71b8372972684aa6cb46d041365e
anilpawale13 <120118798+anilpawale13@users.noreply.github.com>
2023-03-11 13:03:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let myAge = 28;
+//(identifier,assiment operator,declaring variables) current age
+//(identifier) myAge
+//(assiment operator) =
+//(declaring variables OR assin value) 28
+
+let earlyYears = 2;
+//creating variable for the earlyYears and value is 2
+
+earlyYears *= 10.5;
+//using Assignment Operator (multiply and assign)
+//earlyYears = earlyYears * 10.5
+
+let laterYear = myAge - 2;
+//current year subtract by 2
+
+laterYear *= 4;
+//using Assignment Operator (multiply and assign )
+//laterYear = laterYear * 4
+
+console.log(earlyYears);
+
+console.log(laterYear);
+
+
+let myAgeInDogYears = earlyYears + laterYear;
+//earlyYears and laterYear value stored is variable,variable name is myAgeInDogYears
+
+
+let myName = "Anil".toLowerCase();
+
+console.log(`My name is ${myName}.I am ${myAge} years old in human years which is 
+${myAgeInDogYears} years old in dog years.`);
+
+
